d3aee0e434b3ae944b1941036c206c97278d2b27
amir salehi <salehi.amir9@gmail.com>
2021-09-01 15:00:49 +0430

    (feat/store-sample): sample products added to PLP

--- src/store/index.js
+++ src/store/index.js
@@ -20,21 +20,54 @@ export default createStore({
             }
         },
         products: [{
-            "id": 5154511,
-            "title": "کوله پشتی 50 لیتری گوگانا کد 30309",
-            "rating": {
-                "rate": 80,
-                "count": 0
+                "id": 5154511,
+                "title": "کوله پشتی 50 لیتری گوگانا کد 30309",
+                "rating": {
+                    "rate": 80,
+                    "count": 0
+                },
+                "status": "marketable",
+                "images": {
+                    "main": "http://dkstatics-public.digikala.com/digikala-products/89b3060c2b29598a493ec3e5d51c25ece70569fb_1620822493.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
+                },
+                "price": {
+                    "selling_price": 1790000,
+                    "rrp_price": 5490000
+                }
             },
-            "status": "marketable",
-            "images": {
-                "main": "http://dkstatics-public.digikala.com/digikala-products/89b3060c2b29598a493ec3e5d51c25ece70569fb_1620822493.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
+            {
+                "id": 5143892,
+                "title": "ساک ورزشی فوروارد کد FCLT9908",
+                "rating": {
+                    "rate": 0,
+                    "count": 0
+                },
+                "status": "marketable",
+                "images": {
+                    "main": "http://dkstatics-public.digikala.com/digikala-products/460bf82fba3977401af9efdb2daf47ef5aef49d9_1620728567.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
+                },
+                "price": {
+                    "selling_price": 1199000,
+                    "rrp_price": 2990000
+                }
             },
-            "price": {
-                "selling_price": 1790000,
-                "rrp_price": 5490000
-            }
-        }, ],
+            {
+                "id": 5155091,
+                "title": "کیف کمری گوگانا کد GOOG-400400",
+                "rating": {
+                    "rate": 0,
+                    "count": 0
+                },
+                "status": "marketable",
+                "images": {
+                    "main": "http://dkstatics-public.digikala.com/digikala-products/eda0ce297dc1b60b0c490f7cf1c0d9c295f84176_1620825324.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
+                },
+                "price": {
+                    "selling_price": 399000,
+                    "rrp_price": 1690000
+                }
+            },
+        ],
         sort: 22,
         pager: {
             "current_page": 1,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,19 @@
-<template>Home</template>
+<template>
+  <ul>
+    <li v-for="product in products" :key="product.id">
+      {{ product.title }}
+    </li>
+  </ul>
+</template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "Home",
   components: {},
+  computed: {
+    ...mapState(["products"]),
+  },
 };
 </script>
