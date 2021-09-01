34f37c6710370defe3939aaab064fe2f9335c0b8
amir salehi <salehi.amir9@gmail.com>
2021-09-01 14:50:25 +0430

    (fix/404): not found page is now ready

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,10 +1,32 @@
 <template>
-  <diV>404</diV>
+  <h1>404 | Not Found</h1>
+  <p>
+    Unfortunatly, This page is removed or doesn't exist. You can click this
+    <router-link to="/">Link</router-link> to go back Home page.
+  </p>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+h1 {
+  margin: 12rem 0 2rem;
+}
+
+h1,
+p {
+  text-align: center;
+}
+
+a {
+  transition: 200ms all ease-in-out;
+  color: #666;
+  font-weight: bolder;
+}
+
+a:hover {
+  color: #000;
+}
 </style>
