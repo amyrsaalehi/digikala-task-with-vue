7b5f136630688400598565ee0d72b1cf8964716b
amir salehi <salehi.amir9@gmail.com>
2021-09-01 14:39:47 +0430

    feat(views): views are now inited and router works properly

---
+++ src/views/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <diV>404</diV>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Details.vue
@@ -0,0 +1,11 @@
+<template>
+  Details
+  <div>{{ $route.params.title }}</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
