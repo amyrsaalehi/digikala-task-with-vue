427e65c4fd3f1e71466f9c3cb551a5f3ccbe186b
amir salehi <salehi.amir9@gmail.com>
2021-09-01 17:34:14 +0430

    (feat/switch): switch added

--- src/store/index.js
+++ src/store/index.js
@@ -5,89 +5,96 @@ const ls = new SecureLS({ isCompression: false });
 
 export default createStore({
     state: {
-        filters: {
-            "has_selling_stock": {
-                "title": "فقط کالاهای موجود",
-                "options": [{
-                    "title": "کالاهای موجود"
-                }],
-                "type": "switch"
-            },
-            "price": {
-                "title": "محدوده قیمت",
-                "options": {
-                    "min": 0,
-                    "max": 1726074600
+        inits: {
+            filters: {
+                "has_selling_stock": {
+                    "title": "فقط کالاهای موجود",
+                    "options": [{
+                        "title": "کالاهای موجود"
+                    }],
+                    "type": "switch"
                 },
-                "type": "slider"
-            }
+                "price": {
+                    "title": "محدوده قیمت",
+                    "options": {
+                        "min": 0,
+                        "max": 1726074600
+                    },
+                    "type": "slider"
+                }
+            },
+            products: [{
+                    "id": 5154511,
+                    "title": "کوله پشتی 50 لیتری گوگانا کد 30309",
+                    "rating": {
+                        "rate": 80,
+                        "count": 0
+                    },
+                    "status": "marketable",
+                    "images": {
+                        "main": "http://dkstatics-public.digikala.com/digikala-products/89b3060c2b29598a493ec3e5d51c25ece70569fb_1620822493.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
+                    },
+                    "price": {
+                        "selling_price": 1790000,
+                        "rrp_price": 5490000
+                    }
+                },
+                {
+                    "id": 5143892,
+                    "title": "ساک ورزشی فوروارد کد FCLT9908",
+                    "rating": {
+                        "rate": 0,
+                        "count": 0
+                    },
+                    "status": "marketable",
+                    "images": {
+                        "main": "http://dkstatics-public.digikala.com/digikala-products/460bf82fba3977401af9efdb2daf47ef5aef49d9_1620728567.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
+                    },
+                    "price": {
+                        "selling_price": 1199000,
+                        "rrp_price": 2990000
+                    }
+                },
+                {
+                    "id": 5155091,
+                    "title": "کیف کمری گوگانا کد GOOG-400400",
+                    "rating": {
+                        "rate": 0,
+                        "count": 0
+                    },
+                    "status": "marketable",
+                    "images": {
+                        "main": "http://dkstatics-public.digikala.com/digikala-products/eda0ce297dc1b60b0c490f7cf1c0d9c295f84176_1620825324.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
+                    },
+                    "price": {
+                        "selling_price": 399000,
+                        "rrp_price": 1690000
+                    }
+                },
+            ],
+            sort: 22,
+            pager: {
+                "current_page": 1,
+                "total_pages": 500,
+                "total_items": 163034
+            },
         },
-        products: [{
-                "id": 5154511,
-                "title": "کوله پشتی 50 لیتری گوگانا کد 30309",
-                "rating": {
-                    "rate": 80,
-                    "count": 0
-                },
-                "status": "marketable",
-                "images": {
-                    "main": "http://dkstatics-public.digikala.com/digikala-products/89b3060c2b29598a493ec3e5d51c25ece70569fb_1620822493.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
-                },
-                "price": {
-                    "selling_price": 1790000,
-                    "rrp_price": 5490000
-                }
-            },
-            {
-                "id": 5143892,
-                "title": "ساک ورزشی فوروارد کد FCLT9908",
-                "rating": {
-                    "rate": 0,
-                    "count": 0
-                },
-                "status": "marketable",
-                "images": {
-                    "main": "http://dkstatics-public.digikala.com/digikala-products/460bf82fba3977401af9efdb2daf47ef5aef49d9_1620728567.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
-                },
-                "price": {
-                    "selling_price": 1199000,
-                    "rrp_price": 2990000
-                }
-            },
-            {
-                "id": 5155091,
-                "title": "کیف کمری گوگانا کد GOOG-400400",
-                "rating": {
-                    "rate": 0,
-                    "count": 0
-                },
-                "status": "marketable",
-                "images": {
-                    "main": "http://dkstatics-public.digikala.com/digikala-products/eda0ce297dc1b60b0c490f7cf1c0d9c295f84176_1620825324.jpg?x-oss-process=image/resize,m_lfit,h_350,w_350/quality,q_60"
-                },
-                "price": {
-                    "selling_price": 399000,
-                    "rrp_price": 1690000
-                }
-            },
-        ],
-        sort: 22,
-        pager: {
+        searchParams: {
+            "has_selling_stock": true,
+            "price_range": [0, 1726074600],
+            "sort": 22,
             "current_page": 1,
-            "total_pages": 500,
-            "total_items": 163034
-        },
-    },
-    mutations: {},
-    actions: {},
-    getters: {
-        products(state) {
-            return state.products
-        },
-        filters(state) {
-            return state.filters
+
         }
     },
+    mutations: {
+        toggleHasSellingStock(state, payload) {
+            state.searchParams.has_selling_stock = payload;
+            console.log(state.searchParams)
+        }
+    },
+    actions: {},
+    getters: {},
     modules: {},
     plugins: [
         createPersistedState({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
   <h2>Filters</h2>
   <ConditionalLoader :condition="filters">
     <ul class="filter-container">
-      <li>
-        {{
-          `${filters.has_selling_stock.title} - ${filters.has_selling_stock.type}`
-        }}
+      <li class="has-selling-stock-filter">
+        {{ `${filters.has_selling_stock.title}` }}
+        <Switch />
       </li>
-      <li>
+
+      <li class="price-filter">
         {{ `${filters.price.title} - ${filters.price.type}` }}
       </li>
     </ul>
@@ -25,15 +25,16 @@
 
 <script>
 import ConditionalLoader from "../components/shared/ConditionalLoader.vue";
+import Switch from "../components/shared/Switch.vue";
 export default {
   name: "Home",
-  components: { ConditionalLoader },
+  components: { ConditionalLoader, Switch },
   computed: {
     products() {
-      return this.$store.state.products;
+      return this.$store.state.inits.products;
     },
     filters() {
-      return this.$store.state.filters;
+      return this.$store.state.inits.filters;
     },
   },
 };
@@ -47,8 +48,17 @@ ul {
   padding: 2rem 1rem;
 }
 li {
+  display: flex;
   background-color: #eee;
   padding: 1rem 2rem;
   border-radius: 10px;
+}
+
+.has-selling-stock-filter {
+  display: flex;
+  flex-flow: row-reverse nowrap;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
 }
 </style>
